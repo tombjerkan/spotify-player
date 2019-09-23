4de8b4ecc55c4c1f5c6e81312407c8439fa35a69
Thomas Bjerkan <t.bjerkan@gmail.com>
2019-09-23 14:10:32 +0100

    Connect to Spotify Web Playback SDK Player

--- src/App.js
+++ src/App.js
@@ -1,5 +1,36 @@
 import React from "react";
+import Player from "./components/Player";
+import useSpotifyPlayer from "./useSpotifyPlayer";
+
+const apiToken = "enter token here";
 
 export default function App() {
-    return <div>App</div>;
+    const [player, state] = useSpotifyPlayer(apiToken);
+
+    if (player === null || state === null) {
+        return null;
+    }
+
+    return (
+        <Player
+            track={{
+                title: state.track_window.current_track.name,
+                artists: state.track_window.current_track.artists.map(
+                    a => a.name
+                ),
+                imageUri: state.track_window.current_track.album.images[0].url
+            }}
+            isConnected={state !== null}
+            isPlaying={!state.paused}
+            canSkipToPrevious={!state.disallows.skipping_prev}
+            canSkipToNext={!state.disallows.skipping_next}
+            onPlay={() => player.resume()}
+            onPause={() => player.pause()}
+            onSkipToPrevious={() => player.previousTrack()}
+            onSkipToNext={() => player.nextTrack()}
+            positionMs={state.position}
+            durationMs={state.duration}
+            onSeek={ms => player.seek(ms)}
+        />
+    );
 }
---
+++ src/useSpotifyPlayer.js
@@ -0,0 +1,55 @@
+import { useState } from "react";
+
+export default function useSpotifyPlayer(token) {
+    const [player, setPlayer] = useState(null);
+    const [state, setState] = useState(null);
+
+    if (player === null) {
+        const interval = setInterval(() => {
+            if (window.Spotify === undefined) {
+                return;
+            }
+
+            const player = new window.Spotify.Player({
+                name: "Tom's Spotify Player",
+                getOAuthToken: cb => {
+                    cb(token);
+                }
+            });
+
+            player.addListener("initialization_error", ({ message }) => {
+                console.error(message);
+            });
+            player.addListener("authentication_error", ({ message }) => {
+                console.error(message);
+            });
+            player.addListener("account_error", ({ message }) => {
+                console.error(message);
+            });
+            player.addListener("playback_error", ({ message }) => {
+                console.error(message);
+            });
+
+            player.addListener("ready", ({ device_id }) => {
+                console.log("Ready with Device ID", device_id);
+            });
+
+            player.addListener("not_ready", ({ device_id }) => {
+                console.log("Device has gone offline", device_id);
+            });
+
+            player.connect();
+
+            setPlayer(player);
+            clearInterval(interval);
+
+            setInterval(() => {
+                player.getCurrentState().then(s => {
+                    setState(s);
+                });
+            }, 100);
+        }, 1000);
+    }
+
+    return [player, state];
+}
